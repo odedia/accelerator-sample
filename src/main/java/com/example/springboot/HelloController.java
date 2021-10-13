7e00039bd1b49f8a9c69a2152912847ffa8f4109
Oded Shopen <odedia@gmail.com>
2021-10-13 13:00:25 +0300

    Commit

---
+++ src/main/java/com/example/springboot/HelloController.java
@@ -0,0 +1,24 @@
+package com.example.springboot;
+
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+
+	@RequestMapping("/")
+	public String index() {
+        System.out.println("Returning message...");
+        return "Greetings from ODed!";
+	}
+
+    @Bean
+    public HttpTraceRepository htttpTraceRepository() {
+            return new InMemoryHttpTraceRepository();
+    }
+
+}
